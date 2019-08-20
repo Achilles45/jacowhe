738b8f2dbc35e281dc41c15fd8519c6e6ddcb4ab
Achilles45 <achillesusuoyibo7@gmail.com>
2019-08-20 20:47:34 +0100

    Cleared the scalfolding

---
+++ src/components/layouts/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="navbar__wrapper"></div>
+</template>
+<script>
+export default {
+  name: "navbar"
+};
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Navbar />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/layouts/Navbar.vue";
+export default {
+  name: "index",
+  components: {
+    Navbar
+  }
+};
+</script>
